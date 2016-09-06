5e9aef8bdf8e5c6308973518d3a60c5aa774d350
ginofarisano <ginofarisano@gmail.com>
2016-09-07 00:03:32 +0200

    Add facebook configuration

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ var api = new ParseServer({
   appId: process.env.APP_ID || 'myAppId',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-  facebookAppIds: [process.env.FACEBOOK_APP_ID],
+  facebookAppIds: ['175378669534591'],
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
   }
